48c0713bdadc98458fb6c14b28b3676e5f1f3ff4
ojtpurposes <ojtpurposes11@gmail.com>
2026-04-21 17:13:12 +0800

    Revert: Clean up experimental Gradle changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,16 +14,6 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-
-    // Force all Android library sub-projects (e.g. google_mlkit_commons) to compile
-    // with SDK 36 so that android:attr/lStar is available.
-    // MUST be registered here (before evaluationDependsOn) so the callback is
-    // in place when transitive dependencies are evaluated.
-    plugins.withId("com.android.library") {
-        extensions.configure<com.android.build.gradle.LibraryExtension>("android") {
-            compileSdk = 36
-        }
-    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
